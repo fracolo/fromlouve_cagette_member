c425106c680b1f0d3ce9aab64ef2498d9fa9de8d
François COLOMBIER <perso@francois-colombier.fr>
2018-01-13 11:09:18 +0100

    Suppression bloc horaire de la page Mes services

--- html/application/view/home/_includes/members_office.php
+++ html/application/view/home/_includes/members_office.php
@@ -8,27 +8,5 @@
        
         </div>
     </div>
-    <div class="col-xs-12">
-        <h3  class="entete ui horizontal divider"><strong>Horaires</strong></h3>
-        <div class="louve-creneau">
-            <div class="row">
-                <div class="col-md-4 col-xs-12">
-                    <h3>Lundi et Mardi (fermé)</h3>
-                    
-                </div>
-                
-                <div class="col-md-4 col-xs-12">
-                    <h3>Mercredi au samedi</h3>
-                    <p>  <button type="button" class="btn btn-default active">8h00-21h00</button></p>                    
-                </div>
-                <div class="col-md-4 col-xs-12">
-                    <h3>Dimanche (fermé) </h3>
-                    
-                </div>
-
-
-            </div>
-            
-        </div>
-    </div>
+    
 </div>
