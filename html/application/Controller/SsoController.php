7e069270174754b01d84c95d33a4d08ef2c99de3
François COLOMBIER <perso@francois-colombier.fr>
2018-01-27 16:07:56 +0100

    Ajout methode sso logout

--- html/application/Controller/SsoController.php
+++ html/application/Controller/SsoController.php
@@ -7,13 +7,67 @@ use Louve\Model\User;
 
 include $_SERVER['DOCUMENT_ROOT'].'/../application/libs/functions.jsconnect.php';
 
+
+
 class SsoController
 {
+ 
+  
+
     public function __construct()
     {
         $this->session = new Session();
         //TODO ACL and redirect
 
+    }
+
+    private static function getUserImage($id) {
+        $public_photo_dir = '/photos';
+        $photos_dir = __DIR__.'/../../public'. $public_photo_dir;
+        $img_path = '';
+        $hash_id = hash('sha256',$id);
+        try {
+            $jpeg = file_exists($photos_dir.'/'.$hash_id.'.jpeg');
+            $png = file_exists($photos_dir.'/'.$hash_id.'.png');
+            $real_fname = '';
+            if (!$jpeg && !$png) 
+            {
+                $imgcode = file_get_contents(IMAGE_RETRIEVE_URL . $id);
+
+                if (strlen($imgcode) > 1) {
+                    $imgdata = base64_decode($imgcode);
+                    $f = finfo_open();
+                    $mime_type = finfo_buffer($f, $imgdata, FILEINFO_MIME_TYPE);
+                    list($type,$ext) = explode('/',$mime_type);
+                    if (in_array($ext, array('jpeg','png'))) {
+                        $fn = $hash_id.'.'.$ext;
+                        $fp = $photos_dir.'/'.$fn;
+                    
+                        if (file_put_contents($fp,$imgdata)) {
+                            $real_fname = $fn;
+                           
+                        }
+                    }
+                    
+                    
+                }     
+
+                                    
+            } else {
+               
+                $ext = $jpeg?'.jpeg':'.png';
+                $real_fname = $hash_id . $ext;
+               
+               
+            }
+            
+            if (strlen($real_fname) > 1) {
+                $img_path = $public_photo_dir.'/'.$real_fname;
+            }
+        } catch(Exception $e) {
+            //Unable to retrieve photo : it's not so important....
+        }
+        return $img_path;
     }
 
     private static function vanilla_auth() {
@@ -33,14 +87,21 @@ class SsoController
             if (in_array($email, VANILLA_ADMINS)){
                 $roles .= ",administrator";
             }
-
+    
+            $uniqueid = $u_conn->getIdOdoo();
+          
             $name = $u_conn->getFirstName(). ' '.$u_conn->getLastName();
             $user = array (
-                            'uniqueid' => $u_conn->getIdOdoo(),
+                            'uniqueid' => $uniqueid ,
                             'name'=> $name,
                             'email' => $u_conn->getEmail(),
                             'roles' => $roles
                           );
+            $img = self::getUserImage($uniqueid);
+
+            if (strlen($img)>1 && $uniqueid == 608) {
+                $user['photourl'] = 'https://espace-membre.lacagette-coop.fr'.$img;
+            }
            
                         
         }
@@ -60,6 +121,17 @@ class SsoController
         }
     }
 
+    private static function vanilla_logout() {
+         $user = new User();
+        require APP . 'view/_templates/header.php';
+        
+        echo "<div class=container>";
+        require APP . 'view/sso/logout.php';
+        echo "</div>";
+        require APP . 'view/_templates/footer.php';
+    }
+
+
     public function vanilla($string) {
         $elts = explode('?',$string);
         $fn_called = array_shift($elts);
@@ -73,6 +145,7 @@ class SsoController
             case 'subscribe':
             break;
             case 'logout':
+               self::vanilla_logout();
             break;
 
         }
