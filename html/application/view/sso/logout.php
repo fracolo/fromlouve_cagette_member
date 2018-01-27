e27fbd976a52f3d2a7bd2314b66e3f880802642b
François COLOMBIER <perso@francois-colombier.fr>
2018-01-27 16:20:27 +0100

    Template sso logout

---
+++ html/application/view/sso/logout.php
@@ -0,0 +1,6 @@
+<div class="row landing">
+    <div class="louve-box">
+    Vous avez demandé à être déconnecté.e du forum en ligne.<br />
+    Si vous souhaitez également perdre l'identification de l'Espace Membre, <a href="<?php echo URL . 'login/logout/'; ?>">cliquez ici</a>.
+    </div>
+</div>
